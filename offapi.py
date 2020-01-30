81c8ee272f414713110baa133bb9c410db3ededd
Sophie Crespo <51911061+SoCrespo@users.noreply.github.com>
2020-01-30 16:58:27 +0100

    Suppress category and nb attributes of Products

--- offapi.py
+++ offapi.py
@@ -9,22 +9,20 @@ class Products():
     max_nb = pm.MAX_PRODUCTS_NB
 
     def __init__(self, category, nb=max_nb):
-        self.category = category
-        self.nb = nb
-        self.content = self.get_products_by_category()
+        self.content = self.get_products_by_category(category, nb)
 
-    def get_products_by_category(self):
+    def get_products_by_category(self, category, nb):
         '''Call the OpenFoodFact API to retrieve products in selected category.
         Downloaded fields are defined in parameters.py.
         :rtype: list of dictionaries (1 dict = 1 product)
          '''
         url = pm.URL
-        payload = pm.payloadFor(self.category, self.nb)
+        payload = pm.payloadFor(category, nb)
         req = requests.get(url, params=payload)
         return req.json().get('products')
 
 
 if __name__ == "__main__":
-    pat = Products('pate-a-tartiner-sucree', 10)
+    pat = Products('pate-a-tartiner', 10)
 
     print(pat.content)
