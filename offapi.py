2129ebd1b0b031dd43349d4f3a8a03d71963c3ac
Sophie Crespo <51911061+SoCrespo@users.noreply.github.com>
2020-01-30 15:54:14 +0100

    Create Products() in offapi.py

--- offapi.py
+++ offapi.py
@@ -1,23 +1,39 @@
+# -*- coding: utf-8 -*-
+
 import requests
 import json
 import parameters as pm
 
-category = pm.CATEGORIES[0]
-fields = pm.FILTERING_FIELDS
+
+class Products():
+
+    def __init__(self, category):
+        self.category = category
+        self.content = self.get_products_by_category()
+
+    def get_products_by_category(self):
+        '''Call the OpenFoodFact API to retrieve products in selected category.
+        Downloaded fields are defined in parameters.py.
+        :rtype: list of dictionaries (1 dict = 1 product)
+         '''
+        fields = pm.FILTERING_FIELDS
+        url = pm.URL
+        payload = {
+            'action': 'process',
+            'tagtype_0': 'categories',
+            'tag_contains_0': 'contains',
+            'tag_0': self.category,
+            'tagtype_1': 'nutrition_grade',
+            'tag_contains_1': 'contains',
+            'fields': fields,
+            'page_size': 10,
+            'json': 'true',
+        }
+        req = requests.get(url, params=payload)
+        return req.json().get('products')
 
 
-url = 'https://fr.openfoodfacts.org/cgi/search.pl'
-payload = {
-    'action': 'process',
-    'tagtype_0': 'categories',
-    'tag_contains_0': 'contains',
-    'tag_0': category,
-    'tagtype_1': 'nutrition_grade',
-    'tag_contains_1': 'contains',
-    'fields': fields,
-    'json': 'true',
-}
+if __name__ == "__main__":
+    pat = Products('pate-a-tartiner')
 
-req = requests.get(url, params=payload)
-
-
+    print(pat.content)
