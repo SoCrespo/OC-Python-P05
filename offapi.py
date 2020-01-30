f01ca974c50306d999eea908e685b11738928dbe
Sophie Crespo <51911061+SoCrespo@users.noreply.github.com>
2020-01-30 15:10:42 +0100

    Write API call in offapi, add fields in parameters.py

--- offapi.py
+++ offapi.py
@@ -1,24 +1,23 @@
 import requests
 import json
+import parameters as pm
 
-categories_json = requests.get('https://fr.openfoodfacts.org/categories.json')
-categories = json.loads(categories_json.content).get('tags')
-selected_categories = [i.get('name') for i in categories if 'tartiner' in i.get('name')]
-print(selected_categories)
-
+category = pm.CATEGORIES[0]
+fields = pm.FILTERING_FIELDS
 
 
-# url = 'https://fr.openfoodfacts.org/cgi/search.pl'
-# payload = {
-#     'action': 'process',
-#     'tagtype_0': 'categories',
-#     'tag_contains_0': 'contains',
-#     'tag_0': 'produits-a-tartiner',
-#     'tagtype_1': 'nutrition_grade',
-#     'tag_contains_1': 'contains',
-#     'json': 'true',
-# }
+url = 'https://fr.openfoodfacts.org/cgi/search.pl'
+payload = {
+    'action': 'process',
+    'tagtype_0': 'categories',
+    'tag_contains_0': 'contains',
+    'tag_0': category,
+    'tagtype_1': 'nutrition_grade',
+    'tag_contains_1': 'contains',
+    'fields': fields,
+    'json': 'true',
+}
 
-# req = requests.get(url, params=payload)
+req = requests.get(url, params=payload)
 
-# print(req.content[:10000])
+
--- parameters.py
+++ parameters.py
@@ -1,3 +1,6 @@
+URL = 'https://fr.openfoodfacts.org/cgi/search.pl'
+
+
 CATEGORIES = [
     'pates-a-tartiner',
     'matieres-grasses-a-tartiner',
@@ -5,3 +8,14 @@ CATEGORIES = [
     'confitures-de-fruits',
     'confiture-de-lait'
 ]
+
+FILTERING_FIELDS = ','.join([
+    'product_name_fr',
+    'generic_name_fr',
+    'brands',
+    'categories_tags',
+    'url',
+    'stores',
+    'nutrition_grade_fr',
+    'ingredients_text_fr'
+    ])
