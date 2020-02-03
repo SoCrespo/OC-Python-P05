f9a84bd36c49629c7ebb9fc46b92f9b2363ddaf7
Sophie Crespo <51911061+SoCrespo@users.noreply.github.com>
2020-02-03 15:23:58 +0100

    Update database_schema.sql (del store table)

--- database_schema.sql
+++ database_schema.sql
@@ -1,3 +1,9 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
 -- -----------------------------------------------------
 -- Schema openfoodfacts
 -- -----------------------------------------------------
@@ -16,21 +22,8 @@ DROP TABLE IF EXISTS `openfoodfacts`.`category` ;
 
 CREATE TABLE IF NOT EXISTS `openfoodfacts`.`category` (
   `cat_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `cat_name` VARCHAR(50) CHARACTER SET 'utf8' NOT NULL,
+  `cat_name` VARCHAR(50) CHARACTER SET 'utf8' NOT NULL COMMENT 'corresponds to categories field in OFF answer',
   PRIMARY KEY (`cat_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = latin1;
-
-
--- -----------------------------------------------------
--- Table `openfoodfacts`.`store`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `openfoodfacts`.`store` ;
-
-CREATE TABLE IF NOT EXISTS `openfoodfacts`.`store` (
-  `store_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `store_name` VARCHAR(30) CHARACTER SET 'utf8' NOT NULL,
-  PRIMARY KEY (`store_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
 
@@ -42,24 +35,20 @@ DROP TABLE IF EXISTS `openfoodfacts`.`product` ;
 
 CREATE TABLE IF NOT EXISTS `openfoodfacts`.`product` (
   `prod_id` INT(11) NOT NULL AUTO_INCREMENT,
-  `prod_brand` VARCHAR(50) NOT NULL,
-  `prod_name` VARCHAR(50) NOT NULL,
-  `prod_full_name` VARCHAR(50) CHARACTER SET 'utf8' NULL,
-  `prod_nutriscore_index` CHAR(1) NOT NULL,
+  `prod_brand` VARCHAR(50) NOT NULL COMMENT 'Corresponds to brands_tags',
+  `prod_name` VARCHAR(50) NOT NULL COMMENT 'Corresponds to product_name_fr',
+  `prod_full_name` VARCHAR(50) CHARACTER SET 'utf8' NULL COMMENT 'Corresponds to generic_name_fr',
+  `prod_nutriscore_index` CHAR(1) NOT NULL COMMENT 'corresponds to nutriscore_grades',
   `cat_id` INT(11) NOT NULL,
-  `store_id` INT(11) ,
-  `prod_url` VARCHAR(2000) NOT NULL,
-  `prod_ingredients` TEXT(2000) ,
+  `prod_url` VARCHAR(2000) NOT NULL COMMENT 'corresponds to url',
+  `prod_ingredients` TEXT(2000) NULL COMMENT 'corresponds to ingredients_text',
+  `prod_stores` VARCHAR(200) NULL COMMENT 'Corresponds to stores',
   PRIMARY KEY (`prod_id`),
-  INDEX `fk_shop_id` (`store_id` ASC),
   INDEX `fk_product_category` (`cat_id` ASC),
   INDEX `idx_prod_name` (`prod_name` ASC),
   CONSTRAINT `fk_product_category`
     FOREIGN KEY (`cat_id`)
-    REFERENCES `openfoodfacts`.`category` (`cat_id`),
-  CONSTRAINT `fk_shop_id`
-    FOREIGN KEY (`store_id`)
-    REFERENCES `openfoodfacts`.`store` (`store_id`))
+    REFERENCES `openfoodfacts`.`category` (`cat_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1;
 
@@ -89,3 +78,7 @@ CREATE TABLE IF NOT EXISTS `openfoodfacts`.`substition` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
