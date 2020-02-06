a15a9e5a92ef9bc6bd7530768d785a8b92624f84
Sophie Crespo <51911061+SoCrespo@users.noreply.github.com>
2020-02-06 16:54:10 +0100

    Modify get_product_by_categories()

--- openfoodfacts_client.py
+++ openfoodfacts_client.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import requests
-import params as pm
+import config as cf
 
 
 class OpenFoodFactsClient:
@@ -17,21 +17,24 @@ class OpenFoodFactsClient:
         :rtype: list of nb dictionaries (1 dict = 1 product).
 
          '''
-        url = pm.URL
-        payload = pm.payloadFor(category, nb)
+        url = cf.URL
+        payload = cf.payload_for(category, nb)
         req = requests.get(url, params=payload)
         return req.json().get('products')
 
-    def get_products_by_categories(self, categories:list)  -> list:
+    def get_products_by_categories(self, categories, nb):
         '''
         Call get_product_by_category() for a list of categories.
-         '''
-        for category in categories:
-            pass
 
+         '''
+        list = []
+        for category in categories:
+            products = self.get_products_by_category(category, nb)
+            list.extend(products)
+        return list
 
 
 if __name__ == "__main__":
     pr = OpenFoodFactsClient()
-    pat = pr.get_products_by_category('pate-a-tartiner', 10)
+    pat = pr.get_products_by_category('pate-a-tartiner', 20)
     print(pat)
