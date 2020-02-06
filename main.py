5235178405b4883301350a915d5939f1dcccf439
Sophie Crespo <51911061+SoCrespo@users.noreply.github.com>
2020-02-06 16:54:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 # -*- coding: utf-8 -*-
 
 import local_db_manager
+import openfoodfacts_client
 import config as cf
 
 
 connect_params = cf.DB_CONNECTION_PARAMS
 schema = cf.DB_SCHEMA
 categories = cf.CATEGORIES
+nb = cf.MAX_PRODUCTS_NB
 
 
-# DATABASE CREATION 
+# DATABASE CREATION
 db = local_db_manager.LocalDBManager()
 db.connect_to_database(**connect_params)
 db.create_tables(schema)
 
 db.fill_category_table(categories)
+
+# GET PRODUCTS
+off_client = openfoodfacts_client.OpenFoodFactsClient()
+products = off_client.get_products_by_categories(categories, nb)
+print(products)
