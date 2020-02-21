9d645a46c1dbde87b3f460046e6eacc5e1d2de07
Sophie Crespo <51911061+SoCrespo@users.noreply.github.com>
2020-02-21 18:02:06 +0100

    Update with public instances methods

--- main.py
+++ main.py
@@ -4,19 +4,8 @@ import custom_db_manager
 import openfoodfacts_client
 
 
-# ##################################################
-# Database creation
-# ##################################################
-db = custom_db_manager.CustomDBManager()
-db.create_tables()
-db.fill_categories_table()
-db.get_categories_rows()
-
-# ##################################################
-# Data retrieving from API as Product objects
-# ##################################################
 off_client = openfoodfacts_client.OpenFoodFactsClient()
-products = off_client.get_Products_from_API()
+db = custom_db_manager.CustomDBManager()
 
-
-db.insert_products(products)
+db.set_database(off_client.products)
+# db.empty_database()
