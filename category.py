4cecdb4ac05530c210538456f2c29a312a18c71e
Sophie Crespo <51911061+SoCrespo@users.noreply.github.com>
2020-02-24 15:23:49 +0100

    Add __repr__ to Category

--- category.py
+++ category.py
@@ -7,3 +7,10 @@ class Category:
         self.id = id
         self.name = name
         self.full_name = full_name
+
+    def __repr__(self):
+        return self.full_name
+
+
+if __name__ == "__main__":
+    pass
