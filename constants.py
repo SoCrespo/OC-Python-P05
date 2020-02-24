a80a4878e80840d19e7800b44a0439d13e1e0a93
Sophie Crespo <51911061+SoCrespo@users.noreply.github.com>
2020-02-24 15:04:48 +0100

    Edit MAIN_MENU constants

--- constants.py
+++ constants.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
-MAIN_MENU = ['MENU PRINCIPAL',
-             'Rechercher des substituts à un produit',
-             'Afficher les substitutions enregistrées',
-             'Réinitialisation complète',
-             'Quitter'
-             ]
+MAIN_MENU_TITLE = 'MENU PRINCIPAL'
+
+MAIN_MENU_CONTENT = [
+                    'Rechercher des substituts à un produit',
+                    'Afficher les substitutions enregistrées',
+                    'Réinitialisation complète',
+                    'Quitter'
+                    ]
