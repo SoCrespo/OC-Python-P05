36df62ad44dd87effae96d7c2bf65e358f50e2ca
Sophie Crespo <51911061+SoCrespo@users.noreply.github.com>
2020-02-25 15:15:09 +0100

    Del product.id and product.display()

--- product.py
+++ product.py
@@ -4,7 +4,6 @@
 class Product:
 
     def __init__(self, dict):
-        self.cat_id = None
         self.category = dict['category']
         self.brand = dict['brand']
         self.name = dict['name']
@@ -17,11 +16,6 @@ class Product:
 
     def __str__(self):
         return self.name
-
-    def display(self):
-        '''Displays all attributes (name and value) of a product.'''
-        for key, value in self.__dict__:
-            print(f'{key}: {value}')
 
 
 if __name__ == "__main__":
