cb11cd31357b2a9fdf94d0e2b21d34f808944c9c
Sophie Crespo <51911061+SoCrespo@users.noreply.github.com>
2020-02-28 15:58:19 +0100

    Del useless comment

--- custom_db_manager.py
+++ custom_db_manager.py
@@ -134,7 +134,6 @@ class CustomDBManager():
         # https://stackoverflow.com/questions/51700960/runtimeerror-generator-raised-stopiteration-every-time-i-try-to-run-app
         try:
             for item in self.cursor.execute(query, multi=True):
-                # cursor.execute is an iterator if multi=True
                 pass
         except RuntimeError:
             pass
