8aef9e5e81d1f8336dc641a25962d6831e61e31b
Sophie Crespo <51911061+SoCrespo@users.noreply.github.com>
2020-03-03 15:29:04 +0100

    Add doctstring

--- list_of_choice.py
+++ list_of_choice.py
@@ -3,6 +3,10 @@
 
 class ListOfChoice:
 
+    '''Group a list of items (ListOfChoice.content)
+    and the title of this list (ListOfChoice.list).
+    Used by Menu class.'''
+
     def __init__(self, title, content):
         self.title = title
         self.content = content
